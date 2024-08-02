a19544c23f647e4471a472ee2e9803ed8b4882b2
NoName2212013 <rajdeepbarman733@gmail.com>
2024-08-02 16:53:39 +0530

    Final commits

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const connectDB = require('./dbConnection')
+const Ticket = require('./schema');
+const cors = require("cors");
+
+app.use(cors());
+//Middleware for parsing Json
+app.use(express.json());
+//Connecting to Database
+connectDB();
+app.use(express.urlencoded({ extended: false }))
+// creating an api and seperating it.
+app.use("/api", require("./routes"));
+
+port=process.env.PORT;
+
+app.listen(port,()=>{
+    console.log(`App listening to port ${port}`)
+});
